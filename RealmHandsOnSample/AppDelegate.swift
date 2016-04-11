e4887d415febe121cca1cd38bd9bcc31d49635a0
Takuya Aso <milanista224@gmail.com>
2016-04-11 22:35:29 +0900

    TwitterRealm try! Realm

--- RealmHandsOnSample/AppDelegate.swift
+++ RealmHandsOnSample/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
+
+        // インスタンスの生成
+        let realm = try! Realm()
+        print(realm)
+
         return true
     }
 
