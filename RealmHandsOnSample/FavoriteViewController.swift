5b00e0361ba54ee0a069780182cc7fbb2f629a16
Takuya Aso <milanista224@gmail.com>
2016-04-23 21:11:53 +0900

    TwitterRealm Favorites タブの画面にお気に入りツイート表示する実装

--- RealmHandsOnSample/FavoriteViewController.swift
+++ RealmHandsOnSample/FavoriteViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FavoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-
     @IBOutlet weak var favoritesTableView: UITableView!
+
+    var favoritesTweet: Results<TweetModel>?
+    var notificationToken: NotificationToken?
 
     // MARK:- Life Cycle
 
@@ -25,6 +28,18 @@ class FavoriteViewController: UIViewController, UITableViewDelegate, UITableView
         self.favoritesTableView.registerNib(UINib(nibName: "FavoriteCell", bundle: nil), forCellReuseIdentifier: "favoriteCell")
         self.favoritesTableView.rowHeight = UITableViewAutomaticDimension
         self.favoritesTableView.estimatedRowHeight = 90
+
+        // インスタンス生成
+        let realm = try! Realm()
+
+        // 検索条件を追加(お気に入りがTrueのときのものを取得)
+        self.favoritesTweet = realm.objects(TweetModel).filter("favorited = true")
+
+        // こいつが変わったら通知するトークン
+        notificationToken = self.favoritesTweet?.addNotificationBlock({ (Results, Error) -> () in
+            // 更新するものはここに書く
+            self.favoritesTableView.reloadData()
+        })
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -42,17 +57,32 @@ class FavoriteViewController: UIViewController, UITableViewDelegate, UITableView
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 1
+        return self.favoritesTweet?.count ?? 0
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCellWithIdentifier("favoriteCell", forIndexPath: indexPath) as! FavoriteCell
 
-        cell.userNameLabel.text = "UserName"
-        cell.tweetTextView.text = "Test"
-        cell.userIconImageView.image = UIImage(named: "icon_favorites")
+        // お気に入りのツイート indexPath.row に対応する値軍をモデルに展開
+        //  の indexPath.row に対応する値軍をモデルに展開
+        let tweet: TweetModel = self.favoritesTweet![indexPath.row]
 
+        cell.userNameLabel.text = tweet.name
+        cell.tweetTextView.text = tweet.text
+        let urlStr: String = tweet.iconURL
+
+        do {
+            // アイコン画像の URL 指定
+            let url: NSURL = NSURL(string: urlStr)!
+            // NSData として取得
+            let imageData: NSData = try NSData(contentsOfURL: url,
+                                               options: NSDataReadingOptions.DataReadingMappedIfSafe)
+            // 取得した NSData を UIImage に変換
+            cell.userIconImageView.image = UIImage(data: imageData)
+        } catch {
+            print("Exception")
+        }
         return cell
     }
 
